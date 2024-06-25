5957f17c8bc34527107e2884415d1dc99d9d4f2b
Erik Magnusson <erikmagn@student.chalmers.se>
2024-06-25 23:37:14 +0200

    fix8

--- HelperScripts/CreateRpiVENV.bash
+++ HelperScripts/CreateRpiVENV.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 python3.11 -m venv "SW/DMMvenv" 
 python -m pip install --upgrade pip --break-system-packages
-pip3 install -r "SW/DMM/requirements_rpi.txt" 
-ECHO "Finnished setting up venv for development"
+pip3 install -r "SW/DMM/requirements_rpi.txt"
