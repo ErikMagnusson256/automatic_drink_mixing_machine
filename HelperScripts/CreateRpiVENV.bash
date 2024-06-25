2f92fb06a0144e434ea0c1f1b0f9c2e4dd2c994a
Erik Magnusson <erikmagn@student.chalmers.se>
2024-06-25 23:34:37 +0200

    fix system package error

--- HelperScripts/CreateRpiVENV.bash
+++ HelperScripts/CreateRpiVENV.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3.11 -m venv "SW/DMMvenv"
+python3.11 -m venv "SW/DMMvenv" --break-system-packages
 python -m pip install --upgrade pip
-pip3 install -r "SW/DMM/requirements_rpi.txt" --break-system-packages
+pip3 install -r "SW/DMM/requirements_rpi.txt" 
 ECHO "Finnished setting up venv for development"
