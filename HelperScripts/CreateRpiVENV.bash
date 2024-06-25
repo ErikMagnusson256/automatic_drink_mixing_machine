f49634c7036f8f9e831b899e5ef51ba64b27feae
Erik Magnusson <erikmagn@student.chalmers.se>
2024-06-25 23:18:22 +0200

    fix

--- HelperScripts/CreateRpiVENV.bash
+++ HelperScripts/CreateRpiVENV.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3.12 -m venv "SW\DMMvenv"
 SW\DMMvenv\Scripts\activate.bat
-python.exe -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip3 install -r ".\SW\DMM\requirements_rpi.txt"
 ECHO "Finnished setting up venv for development"
