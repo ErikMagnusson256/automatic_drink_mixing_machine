bdab9e76bd8f4276c749a1eb8239268ea8061b2a
Erik Magnusson <erikmagnusson256@gmail.com>
2024-06-26 22:38:03 +0100

    Add from rpi

--- HelperScripts/CreateWindowsVENV.bat
+++ HelperScripts/CreateWindowsVENV.bat
@@ -2,5 +2,5 @@ ECHO "Starting setting up venv for development"
 python3.12 -m venv "SW\DMMvenv"
 SW\DMMvenv\Scripts\activate.bat
 python.exe -m pip install --upgrade pip
-pip3 install -r ".\SW\DMM\requirements_windows.txt"
+pip3 install -r ".\SW\DMM\requirements.txt"
 ECHO "Finnished setting up venv for development"
--- SW/DMM/DMM/guitest.py
+++ SW/DMM/DMM/guitest.py
@@ -1,5 +1,11 @@
 import sys
 import os 
+import signal
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication, QDialog
 from PySide6.QtCore import QFile, QIODevice
@@ -7,6 +13,10 @@ from PySide6 import QtWidgets
 from PySide6 import QtCore, QtGui, QtUiTools
 from pyside_dynamic import *
 
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    sys.stderr.write('\r')
+    QApplication.quit()
 
 #def loadUiWidget(uifilename, parent=None):
 #    loader = QtUiTools.QUiLoader()
@@ -19,7 +29,8 @@ from pyside_dynamic import *
 class Screen1(QDialog):
     def __init__(self):
         super(Screen1, self).__init__()
-        loadUi("../../../GUI/UI/screen1.ui", self)
+        print(os.path.dirname(os.path.realpath(__file__)))
+        loadUi("./../../../GUI/UI/screen1.ui", self)
 
         self.pushButton.clicked.connect(self.gotoscreen2)
 
@@ -29,13 +40,14 @@ class Screen1(QDialog):
 class Screen2(QDialog):
     def __init__(self):
         super(Screen2, self).__init__()
-        loadUi("../../../GUI/UI/screen2.ui", self)
+        loadUi("./../../../GUI/UI/screen2.ui", self)
 
         self.pushButton.clicked.connect(self.gotoscreen1)
     
     def gotoscreen1(self):
         widget.setCurrentIndex(widget.currentIndex() - 1)
         #widget.setCurrentWidget(Screen1Obj) # <------
+
 
 if __name__ == "__main__":
     # ---- load ui file -----
@@ -55,8 +67,14 @@ if __name__ == "__main__":
     #window.show()
     #sys.exit(app.exec())
 
+    signal.signal(signal.SIGINT, signal_handler)
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(True)
+    
     widget = QtWidgets.QStackedWidget() # a stack of widgets
+
+    widget.setFixedWidth(800)
+    widget.setFixedHeight(600)
 
     Screen1Obj = Screen1()
     Screen2Obj = Screen2()
@@ -69,6 +87,8 @@ if __name__ == "__main__":
 
     print("index of 2nd widget is=", widget.indexOf(Screen1Obj))
 
+    #widget.showFullScreen()
     widget.show()
-
+    
     sys.exit(app.exec())
+    app.exec_()
